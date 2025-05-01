35322e586e123b8bcb54de2e47d945a9801a3a5e
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-05-02 00:07:23 +0600

    start

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import LayOutOne from "./LayOut/LayOutOne";
+import Home from "./Pages/Home";
+
+function App() {
+  const myRoute = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<LayOutOne />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Route>
+    )
+  );
+
+  return (
+    <>
+      <RouterProvider router={myRoute} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav className="NavBar">
+        <div className="container">
+            
+        </div>
+
+        
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
